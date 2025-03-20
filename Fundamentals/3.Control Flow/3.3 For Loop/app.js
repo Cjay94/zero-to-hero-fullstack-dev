414615e7cc72496f5c7074068b9b46e495c8fedc
Cjay94 <cjephthah94@gmail.com>
2025-03-20 16:10:37 +0200

    JS For Loop

---
+++ Fundamentals/3.Control Flow/3.3 For Loop/app.js
@@ -0,0 +1,15 @@
+// for (initialExpression; condition; incrementExpression) {}
+
+// for(let x = 0; x <= 5; x++ ){
+// console.log(x);
+// }
+
+
+//Q1 Print your name & numbers from 0 through 1000
+
+// A1
+let name = "Bill";
+
+for (let i = 0; i <= 1000; i++) {
+    console.log(name, i); //Bill 0 --> Bill 1000
+}
