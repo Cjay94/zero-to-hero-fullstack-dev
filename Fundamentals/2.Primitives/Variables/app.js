69557929cbf589b5d223a98dca317bfbf9fc106c
Cjay94 <cjephthah94@gmail.com>
2025-03-20 14:06:58 +0200

    How to declare Variables in JS

---
+++ Fundamentals/2.Primitives/Variables/app.js
@@ -0,0 +1,39 @@
+// (reserved keyword): (variable name): (type of value)
+
+//var --> old way of declaring variable
+
+var car; //declared a variable 
+console.log(car); // undefined
+
+let alias = "CJD"; //name variable assigned a value
+console.log(alias);
+
+const pi = 3.14; // number value variable  assigned 
+console.log(pi);
+
+
+// Q1. Create variable name (name) & store your name.
+// Q2. Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+// Q3. Create variable name (gender) & store your gender.
+// Q4. Create variable name (twitterHandle) & store your twitter handle.
+// Q5. Finally log all variables to the console.
+
+
+
+
+//A1
+let name = "Jim";
+console.log(name);
+
+//A2
+const whatDoYouWannaBecomeInYourLife = "Developer";
+console.log(whatDoYouWannaBecomeInYourLife);
+
+//A3
+
+const gender = "male"
+console.log(gender);
+
+//A4
+let twitterHandle = "@CJD"
+console.log(twitterHandle);
