35cff44c382456ecd7beab298a56f849f10a9d54
Cjay94 <cjephthah94@gmail.com>
2025-03-20 14:19:13 +0200

    JS Numbers

---
+++ Fundamentals/2.Primitives/2.2Numbers/app.js
@@ -0,0 +1,31 @@
+// JavaScript has one number type
+// Positive Number ie 7
+// Negative Number ie -5
+// Whole Number (integers) ie 3.14
+// Decimal Numbers ie 0.5
+
+// Q1. Create Variable name (firstFavNum) & store your favorite number.
+// Q2. Create Variable name (secondFavNum) & store your second fav number.
+// Q3. Add (firstFavNum & secondFavNumber).
+// Q4. Subtract (firstFavNum & secondFavNumber).
+// Q5. Multiply (firstFavNum & secondFavNumber).
+// Q6. Divided (firstFavNum & secondFavNumber).
+// Q7. Check (firstFavNum Mod secondFavNumber)
+// Q8. Check the power of (firstFavNum)
+
+// A1
+const firstFavNum = 6;
+// A2
+const secondFavNum = 24;
+// A3
+console.log(firstFavNum + secondFavNum);
+// A4
+console.log(firstFavNum - secondFavNum);
+// A5
+console.log(firstFavNum * secondFavNum);
+// A6
+console.log(firstFavNum / secondFavNum);
+// A7
+console.log(firstFavNum % secondFavNum);
+// A8
+console.log(firstFavNum ** secondFavNum);
