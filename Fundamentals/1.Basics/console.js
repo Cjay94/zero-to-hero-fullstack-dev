1ebe357c73cafb3fc506e7af805c6d3ae57de97b
Cjay94 <cjephthah94@gmail.com>
2025-03-20 12:18:39 +0200

    Console & Comments in JS

---
+++ Fundamentals/1.Basics/console.js
@@ -0,0 +1,33 @@
+// ---------- CONSOLE ----------
+console.log("Hello CJD");
+console.error("This is an Error");
+console.warn("This might not work");
+console.info("Here is some great news");
+console.table({
+    name: "CJD",
+    age: 30,
+    location: ["RSA"]
+})
+// ---------- CONSOLE SHORTCUTS ----------
+// log --> console.log();
+// error --> console.error();
+// warn --> console.warn();
+// clg-- > Displays a message in the console: console.log(first)
+// console.log(console); --> console object
+
+// ---------- COMMENTS ----------
+
+// This is one line comment
+
+/*
+    This is 
+    multi-line 
+        comment
+*/
+
+/** Unordered Comment
+ * bla
+ * bla
+ * bla
+ */
+
