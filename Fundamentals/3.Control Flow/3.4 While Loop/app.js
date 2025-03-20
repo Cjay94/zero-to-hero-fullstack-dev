2b9e3ff3d85fbe3440e891c46d71b9271cc9295b
Cjay94 <cjephthah94@gmail.com>
2025-03-20 16:17:24 +0200

    JS While Loop

---
+++ Fundamentals/3.Control Flow/3.4 While Loop/app.js
@@ -0,0 +1,24 @@
+// while (condition) {
+//   // code block to be executed
+// }
+
+// ----------------
+let i = 1;
+
+while (i <= 5) {
+    console.log("Hello World", i);
+    i++;
+}
+// ----------------
+
+//Q1 Iteration will start from 10.
+//Q2 Print "Your Name" 100 times to the console using while loop
+
+//A1
+let j = 10;
+
+//A2 
+while (j <= 100) {
+    console.log("Ben", j);
+    j++; // Ben 10 --> Ben 100
+}
